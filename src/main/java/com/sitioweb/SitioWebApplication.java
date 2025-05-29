72319fefbaa645adf34bb787b7684dc51314d796
ashjiga <ashjiga11@gmail.com>
2025-05-28 19:05:50 -0600

    First Commit

---
+++ src/main/java/com/sitioweb/SitioWebApplication.java
@@ -0,0 +1,13 @@
+package com.sitioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitioweb/SitioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
